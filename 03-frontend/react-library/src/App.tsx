f769dc5b6cb16c61fcc20b1202604268e120b1bc
Will Reed <wreed3rd@gmail.com>
2024-03-04 22:29:46 -0500

    code clean for homepage -wr

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { Heros } from './layouts/HomePage/Heros';
-import { LibraryServices } from './layouts/HomePage/LibraryServices';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
 export const App = () => {
 	return (
 		<div>
 			<Navbar />
-			<ExploreTopBooks />
-			<Carousel />
-			<Heros />
-			<LibraryServices />
+			<HomePage />
 			<Footer />
 		</div>
 	);
