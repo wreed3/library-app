09a86293d03d4bddbf03f61d89f3a33a946570bc
Will Reed <wreed3rd@gmail.com>
2024-03-08 14:33:07 -0500

    added react-router-dom and some links -wr

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { ReturnBook } from './ReturnBook';
 import React, { useEffect, useState } from 'react';
 import { BookModel } from '../../../models/BookModel';
 import { SpinnerLoading } from '../../../Utils/SpinnerLoading';
+import { Link } from 'react-router-dom';
 
 export const Carousel = () => {
 	const [books, setBooks] = useState<BookModel[]>([]);
@@ -130,9 +131,9 @@ export const Carousel = () => {
 				</div>
 			</div>
 			<div className="homepage-carousel-title mt-3">
-				<a className="btn btn-outline-secondary btn-lg" href="#void">
+				<Link className="btn btn-outline-secondary btn-lg" to="/search">
 					View More
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
