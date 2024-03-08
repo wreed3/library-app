002c56e0c8d6ff0f868f25d0eb22a7e6fa535999
Will Reed <wreed3rd@gmail.com>
2024-03-07 20:25:41 -0500

    add search by title -wr

--- 02-backend/spring-boot-library/src/main/java/com/libraryyourway/springbootlibrary/entity/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/libraryyourway/springbootlibrary/entity/dao/BookRepository.java
@@ -1,8 +1,13 @@
 package com.libraryyourway.springbootlibrary.entity.dao;
 
 import com.libraryyourway.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 
 }
