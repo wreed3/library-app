632015d41463fc121e9a1ad54576c3896b7737ac
Will Reed <wreed3rd@gmail.com>
2024-03-09 13:06:43 -0500

    added review entity, review repository, and config to limit Rest calls -wr

--- 02-backend/spring-boot-library/src/main/java/com/libraryyourway/springbootlibrary/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-library/src/main/java/com/libraryyourway/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.libraryyourway.springbootlibrary.config;
 
 import com.libraryyourway.springbootlibrary.entity.Book;
+import com.libraryyourway.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -16,8 +17,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /*Configure CORS Mapping*/
         String theAllowedOrigins = "http://localhost:3000";
---
+++ 02-backend/spring-boot-library/src/main/java/com/libraryyourway/springbootlibrary/entity/dao/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.libraryyourway.springbootlibrary.entity.dao;
+
+import com.libraryyourway.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findByBookId(@RequestParam("bookId") Long bookId, Pageable pageable);
+}
