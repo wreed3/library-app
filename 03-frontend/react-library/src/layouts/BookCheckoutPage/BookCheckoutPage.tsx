112151a76f06aa8eaaeffc23e98e3df18df03c16
Will Reed <wreed3rd@gmail.com>
2024-03-08 19:05:27 -0500

    attempt to fix git tracking -wr

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { BookModel } from '../../models/BookModel';
 import { SpinnerLoading } from '../../Utils/SpinnerLoading';
 import { useParams } from 'react-router-dom';
+import { StarsReview } from '../../Utils/StarsReview';
 
 export const BookCheckoutPage = () => {
 	const [book, setBook] = useState<BookModel>();
@@ -77,6 +78,7 @@ export const BookCheckoutPage = () => {
 							<h2>{book?.title}</h2>
 							<h5 className="text-primary">{book?.author}</h5>
 							<p className="lead">{book?.description}</p>
+							<StarsReview rating={4.5} size={32} />
 						</div>
 					</div>
 				</div>
@@ -100,6 +102,7 @@ export const BookCheckoutPage = () => {
 						<h2>{book?.title}</h2>
 						<h5 className="text-primary">{book?.author}</h5>
 						<p className="lead">{book?.description}</p>
+						<StarsReview rating={4.5} size={32} />
 					</div>
 				</div>
 				<hr />
